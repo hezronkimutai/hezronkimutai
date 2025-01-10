6abd141ef747c20be48e5da8a316478e42c38983
Hezzie <hezronchelimo.hc@gmail.com>
2025-01-10 16:35:24 +0300

    test coverage

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
+
 test('Renders Landing Page', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hezron Kimutai/i);
