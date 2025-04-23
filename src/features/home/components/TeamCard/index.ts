744398b36e9c51fda3af61582b388e0832adfbd9
hezronkimutai <hezronchelimo.hc@gmail.com>
2025-04-23 23:03:54 +0300

    refactor(team): remove Team and TeamCard components along with associated assets and tests

--- src/features/home/components/TeamCard/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as TeamCard } from './TeamCard';
-export type { TeamCardProps } from './TeamCard';
--- src/features/home/components/index.ts
+++ src/features/home/components/index.ts
@@ -2,8 +2,6 @@ export * from './CallToActionBtn';
 export * from './LandingDiv';
 export * from './Projects';
 export * from './Services';
-export * from './Team';
-export * from './TeamCard';
 export * from './WhyMe';
 export * from './Footer';
 export * from './PaymentButton';
